892283022bae351c0a6cad28458623f6bb039c80
Anika Hussen <anika.hussen@gmail.com>
2019-08-07 15:28:58 -0400

    new update

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -5,6 +5,12 @@ public class MapDisplay : MonoBehaviour
 
     public Renderer textureRender;
 
+    public void DrawTexture(Texture2D texture)
+    {
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
     public void DrawNoiseMap(float[,] noiseMap)
     {
         int width = noiseMap.GetLength(0);
